fea2d0a9f58420fd2663bf3948c40efadfc9b242
Pete A <peter.ayresmusic@gmail.com>
2019-03-01 19:48:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
 end
